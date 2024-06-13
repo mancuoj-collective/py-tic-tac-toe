b89e9028ce1548a1db30a5b9d58790266a50a335
mancuoj <mancuoj@gmail.com>
2024-06-14 00:37:39 +0800

    feat: game state

--- src/tic_tac_toe/models/game_state.py
+++ src/tic_tac_toe/models/game_state.py
@@ -1,8 +1,20 @@
+import re
 from dataclasses import dataclass
 from functools import cached_property
 
 from .grid import Grid
 from .mark import Mark
+
+WINNING_PATTERNS = (
+    "???......",
+    "...???...",
+    "......???",
+    "?..?..?..",
+    ".?..?..?.",
+    "..?..?..?",
+    "?...?...?",
+    "..?.?.?..",
+)
 
 
 @dataclass(frozen=True)
@@ -16,3 +28,23 @@ class GameState:
             return self.starting_mark
         else:
             return self.starting_mark.other
+
+    @cached_property
+    def game_not_started(self) -> bool:
+        return self.grid.empty_cells == 9
+
+    @cached_property
+    def game_over(self) -> bool:
+        return self.winner is not None
+
+    @cached_property
+    def tie(self) -> bool:
+        return self.winner is None and self.grid.empty_count == 0
+
+    @cached_property
+    def winner(self) -> Mark:
+        for pattern in WINNING_PATTERNS:
+            for mark in Mark:
+                if re.match(pattern.replace("?", mark), self.grid.cells):
+                    return mark
+        return None
