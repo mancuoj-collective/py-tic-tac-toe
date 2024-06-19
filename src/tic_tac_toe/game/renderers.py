f0c00f93da3c577b757cdb6c3acef7057836e40b
mancuoj <mancuoj@gmail.com>
2024-06-20 00:20:16 +0800

    chore: update

--- src/tic_tac_toe/game/renderers.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-from tic_tac_toe.models.game_state import GameState
-
-
-class Renderer(ABC):
-    @abstractmethod
-    def render(self, game_state: GameState) -> None:
-        """Render the current game state."""
